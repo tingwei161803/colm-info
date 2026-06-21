ec6180704744f947cf4a854551ac6b7baeee0f8e
tingwei161803 <monkeydluffy3u4ace@gmail.com>
2026-06-21 23:22:46 +0800

    Make English the default page language

--- assets/app.js
+++ assets/app.js
@@ -39,7 +39,7 @@
 
   /* ---------- global state ---------- */
   var state = {
-    lang:  lsGet("lang")  || "zh",       // default language: zh
+    lang:  lsGet("lang")  || "en",       // default language: zh
     theme: lsGet("theme") || "light"
   };
 
